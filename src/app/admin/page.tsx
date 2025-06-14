60748f8b7d357284e94f267d4cb3e889d4e0ca78
ppt4pq <ppt4pq@virginia.edu>
2025-06-13 21:29:04 -0400

    fixed issue hopefully

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,8 @@
+export default function AdminPage() {
+    return (
+      <div>
+        <h1>Admin Dashboard</h1>
+        <p>Protected admin area for blog management.</p>
+      </div>
+    );
+  }
