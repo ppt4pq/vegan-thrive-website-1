980b8cb4daa361daec4d8edd89fe47386b705e90
ppt4pq <ppt4pq@virginia.edu>
2025-06-30 13:50:16 -0400

    vercel please work

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -3,27 +3,31 @@ import NotionClientRenderer from '@/components/NotionClientRenderer';
 import { type Metadata } from 'next';
 import { notFound } from 'next/navigation';
 
-type Props2 = {
-  params: { slug: string };
-};
-
 export async function generateStaticParams() {
   const posts = await getPublishedPosts();
   return posts.map((post) => ({ slug: post.slug }));
 }
 
-export async function generateMetadata({ params }: Props2): Promise<Metadata> {
+export async function generateMetadata({
+  params,
+}: {
+  params: { slug: string };
+}): Promise<Metadata> {
   return {
     title: params.slug,
   };
 }
 
-export default async function BlogPostPage({ params }: Props2) {
+export default async function BlogPostPage({
+  params,
+}: {
+  params: { slug: string };
+}) {
   const posts = await getPublishedPosts();
   const post = posts.find((p) => p.slug === params.slug);
 
   if (!post) {
-    notFound(); // Better than a <div> for 404
+    notFound();
   }
 
   const recordMap = await getPage(post.id);
