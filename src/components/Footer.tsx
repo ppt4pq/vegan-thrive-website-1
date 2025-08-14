118aac0ed2ad88bb311d2db7b4ac6de997b332cc
ppt4pq <ppt4pq@virginia.edu>
2025-08-14 14:11:18 -0400

    edited footer to look better

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,11 +2,10 @@ import Link from 'next/link';
 
 export default function Footer() {
     return (
-        <footer className="bg-gray-100 flex flex-wrap items-center justify-between py-5 px-10 border-t border-gray-300 bottom-0">
-            <p className="w-full md:w-1/3 mb-3 md:mb-0 text-gray-500 text-center md:text-left">
+        <footer className="bg-transparent h-20 flex flex-wrap items-center justify-between py-5 px-10 border-t text-sm border-gray-300 bottom-0">
+            <p className="w-full md:w-1/3 mb-3 md:mb-0 text-gray-500 text-sm md:text-left">
                 © 2025 Vegan Thrive Coaching
             </p>
-            <Link href="/" className="w-full md:w-1/3 flex items-center justify-center mb-3 md:mb-0">Home</Link>
             <ul className="w-full md:w-1/3 flex justify-center md:justify-end space-x-4 text-gray-500">
                 <li><Link href="#" className="hover:text-gray-700">Home</Link></li>
                 <li><Link href="#" className="hover:text-gray-700">Blog</Link></li>
