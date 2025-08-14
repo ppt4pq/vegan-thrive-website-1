b89bd4ced4c4ad9470cc233532e31010e978e00c
ppt4pq <ppt4pq@virginia.edu>
2025-08-14 14:30:41 -0400

    made blog cards into links

--- src/components/BlogCard.tsx
+++ src/components/BlogCard.tsx
@@ -9,6 +9,7 @@ interface BlogCardProps {
 
 export default function BlogCard({ title, date, excerpt, slug }: BlogCardProps) {
     return ( 
+        <Link href={slug ? `/blog/${slug}` : "/blog"} className='block group'>
         <div className='group relative border-green-700 border-1 p-10 h-90 w-90 transition-all duration-0 hover:bg-[#003000] hover:text-white'>
             <div className='flex flex-col h-full'>
             <h2 className='text-2xl'>{title}</h2>
@@ -16,5 +17,6 @@ export default function BlogCard({ title, date, excerpt, slug }: BlogCardProps) 
             <div className="absolute bottom-0 left-0 w-full h-20 mb-10 bg-gradient-to-t from-white to-transparent pointer-events-none transition-all duration-0 group-hover:bg-gradient-to-t group-hover:from-[#003000] group-hover:to-transparent" />
             </div>
         </div>
+        </Link>
     )
 }
