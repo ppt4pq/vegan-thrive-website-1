9ac784de5656f55bae851e139dfcedb61ff31645
ppt4pq <ppt4pq@virginia.edu>
2025-08-16 16:40:15 -0400

    added contact page, ready to share publicly

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,13 @@
+export default async function ContactPage() {
+    return (
+        <div className="mx-auto text-center p-12 justify-end items-start min-h-screen bg-cover bg-center flex" style={{ backgroundImage: "url('/images/rapids.jpeg')" }}>
+            <div className="bg-white/40 max-w-md text-left p-5 mt-24 me-20">
+                <h1 className="text-lg mb-2">Contact us.</h1>
+                <ul>
+                    <li>Email: kayla@veganthrivecoaching.com</li>
+                    <li>Instagram: @veganthriveclub</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
